872873c6d785f471d5c9d43db84bc660805f0ebd
js-code-ua <js.code.ua@gmail.com>
2020-01-12 21:36:44 +0200

    create user logic

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import * as bcrypt from 'bcrypt';
+import * as _ from 'lodash';
+
+import { IUser } from './interfaces/user.interface';
+import { Model } from 'mongoose';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(@InjectModel('User') private readonly userModel: Model<IUser>){}
+
+    async create(createUserDto: CreateUserDto, roles: Array<string>): Promise<IUser> {
+        const saltRounds = 10;
+        const salt = await bcrypt.genSalt(saltRounds);
+        const hash = await bcrypt.hash(createUserDto.password, salt);
+
+        const createdUser = new this.userModel(_.assignIn(createUserDto, { password: hash, roles }));
+        return await createdUser.save();
+    }
+
+    async find(id: string): Promise<IUser> {
+        return await this.userModel.findById(id).exec();
+    }
+}
